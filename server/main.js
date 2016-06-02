36bfd4822873b1d7eee1b5f3d725ebd4c8c5bc79
Rafał Bazarnik <rafal.bazarnik@blstream.com>
2016-06-02 15:39:49 +0200

    first seven steps in tutorial

---
+++ server/main.js
@@ -0,0 +1,11 @@
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+// import '../imports/api/tasks.js';
+// import { Tasks } from '../imports/api/tasks.js';
+import { Tasks } from '../imports/ui/App.jsx'
+
+Meteor.startup(() => {
+  // code to run on server at startup
+});
+//
+// Tasks = new Mongo.Collection('tasks');
